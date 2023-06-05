f098c8357cfba23d7d4e7721faafeac424b0c7b8
chathura456 <chathuracde@gmail.com>
2023-06-06 00:35:21 +0530

    partially done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.cameras});
@@ -22,12 +23,21 @@ class _HomePageState extends State<HomePage> {
           if(!isWorking){
             isWorking = true,
             cameraImage = image,
-            //runModel()
+             //runModel()
           }
         });
       });
     });
   }
+
+  loadModel() async{
+    await Tflite.loadModel(
+        model: 'assets/mobilenet_v1_1.0_224.tflite',
+        labels: 'assets/mobilenet_v1_1.0_224.txt'
+    );
+
+  }
+
 
   @override
   void initState() {
@@ -35,6 +45,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     cameraController = CameraController(widget.cameras[0],ResolutionPreset.high);
     initCamera();
+    //loadModel();
 
   }
 
@@ -42,17 +53,50 @@ class _HomePageState extends State<HomePage> {
   Future<void> dispose() async {
     // TODO: implement dispose
     super.dispose();
-    //await Tflite.close();
+    await Tflite.close();
+    cameraController.stopImageStream();
     cameraController.dispose();
+  }
+
+  Future runModel() async {
+    if(cameraImage != null){
+      await loadModel();
+      print('--------checked----------------');
+      var recognitions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage!.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: cameraImage!.height,
+          imageWidth: cameraImage!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true
+      );
+      results = "";
+      recognitions?.forEach((response) {
+        results += response["label"] + " " + (response["confidence"] as double).toStringAsFixed(2) + "\n\n";
+        print(results);
+      });
+      setState(() {
+        results;
+      });
+      isWorking = false;
+    }
   }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(title: const Text('Object Detection'),),
       body: Container(
+
         decoration: const BoxDecoration(
+            color: Colors.white,
             image: DecorationImage(
                 image: AssetImage('assets/images/jarvis.jpg')
             )
@@ -71,8 +115,10 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Center(
                   child: ElevatedButton(
-                    onPressed: () {
+                    onPressed: () async {
+                      cameraController.stopImageStream();
                       initCamera();
+                      await runModel();
                     },
                     child: Container(
                       margin: const EdgeInsets.only(top: 35),
@@ -81,7 +127,7 @@ class _HomePageState extends State<HomePage> {
                       child: cameraImage ==null? const SizedBox(
                         height: 50,
                         width: 50,
-                        child: Icon(Icons.photo_camera_front,color: Colors.blueAccent,size: 40,),
+                        child: Icon(Icons.photo_camera_front,color: Colors.white,size: 40,),
                       ):AspectRatio(
                         aspectRatio: cameraController.value.aspectRatio,
                         child: CameraPreview(cameraController),
